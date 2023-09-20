960078f46057cf02df8848601307bdd36884a60e
unknown <gausulislam5@gmail.com>
2023-09-20 22:52:49 +0600

    update section

--- js/scripr.js
+++ js/scripr.js
@@ -37,19 +37,38 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
     const previousTotalExpensiveBalanceStrings = totalExpensiveBalance.innerText;
     const previousTotalExpensiveBalance = parseFloat(previousTotalExpensiveBalanceStrings);
 
-    const incomeExpensiveBalanceTotal = newFoodField + newRentField + newClothesField+previousTotalExpensiveBalance;
-    totalExpensiveBalance.innerText=incomeExpensiveBalanceTotal;
+    const incomeExpensiveBalanceTotal = newFoodField + newRentField + newClothesField + previousTotalExpensiveBalance;
+    totalExpensiveBalance.innerText = incomeExpensiveBalanceTotal;
 
-const totalBalanceElements1 = document.getElementById('total-balance');
-const previousTotalBalanceStrings1 = totalBalanceElements1.innerText;
-const previousTotalBalance1 = parseFloat(previousTotalBalanceStrings1);
+    const totalBalanceElements1 = document.getElementById('total-balance');
+    const previousTotalBalanceStrings1 = totalBalanceElements1.innerText;
+    const previousTotalBalance1 = parseFloat(previousTotalBalanceStrings1);
+
+    const totalMainBalance = previousTotalBalance1 - incomeExpensiveBalanceTotal;
+    totalBalanceElements1.innerText = totalMainBalance;
+})
+
+document.getElementById('btn-save').addEventListener('click',function(){
+    const savingField = document.getElementById('saving-field');
+    const newSavingFieldString = savingField.value;
+    const newIncomeField = parseFloat(newSavingFieldString);
+
+    console.log(newIncomeField);
+    const totalBalanceElements1 = document.getElementById('total-balance');
+    const previousTotalBalanceStrings1 = totalBalanceElements1.innerText;
+    const previousTotalBalance1 = parseFloat(previousTotalBalanceStrings1);
+
+    // const savingBalance = totalMainBalance * 0.2;
+    // console.log(savingBalance);
 
 
-const totalMainBalance= previousTotalBalance1 - incomeExpensiveBalanceTotal;
+    const savingAmount = document.getElementById('saving-amount');
+    const previousSavingAmountStrings = savingAmount.innerText;
+    const previousSavingAmount = parseFloat(previousSavingAmountStrings);
 
-totalBalanceElements1.innerText=totalMainBalance;
-
-
+    const remainingBalance = document.getElementById('remaining-balance');
+    const previousRemainingBalanceStrings = remainingBalance.innerText;
+    const previousRemainingBalance = parseFloat(previousRemainingBalanceStrings);
 
 
 
